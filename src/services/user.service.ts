634fd7b078e4899d373a591c705fd2cd4bc15b6a
natan <natanbarbosax193@gmail.com>
2025-07-31 19:27:45 -0300

    feat: add user service with find method

---
+++ src/services/user.service.ts
@@ -0,0 +1,24 @@
+import { UserEntitie, userRole } from "@/entities/User.entitie";
+
+export class UserService {
+  private readonly users = [
+    new UserEntitie(
+      1,
+      "Fake Name 1",
+      "fakeemail1@test.com.br",
+      "12345678",
+      userRole.admin
+    ),
+    new UserEntitie(
+      2,
+      "Fake Name 2",
+      "fakeemail2@test.com.br",
+      "87654321",
+      userRole.user
+    ),
+  ];
+
+  public findUserByEmail(email: string) {
+    return this.users.find((user) => user.email == email);
+  }
+}
